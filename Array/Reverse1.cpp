f0bc91683750c064c30e79bb31c55a3eee4e1e4e
Rupal Das <60324778+rupal11@users.noreply.github.com>
2020-09-24 21:53:40 +0530

    Add files via upload

---
+++ Array/Reverse1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void Reverse1(int *A,int n)
+{
+    int i,j,B[n];
+    for(i=0,j=n-1 ; i<n ; ++i,--j)
+     B[i]=A[j];
+    for(i=0;i<n;++i)
+     A[i]=B[i];
+    return ;
+}
+
+int main() {
+    int i,k,n,index;
+	cout<<"Enter the value of n :";
+	cin>>n;
+	int A[n];
+	for(i=0;i<n;++i)
+	{
+    	 cout<<"Enter the value : ";
+    	 cin>>A[i];
+    }
+    Reverse1(A,n);
+	cout<<"The reverse array is : \n";
+    for(i=0;i<n;++i)
+     cout<<A[i]<<endl;
+	return 0;
+}
